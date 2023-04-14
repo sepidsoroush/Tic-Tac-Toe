5c906fb4a75da60d9ee04cb7f71b21824b916377
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-14 12:08:23 +0300

    create Board of game

---
+++ src/components/Board.js
@@ -0,0 +1,30 @@
+const Square = ({value})=>{
+    function handleClick(){
+        console.log('clicked!')
+    }
+    return(
+        <button onClick={handleClick}>{value}</button>
+    )
+}
+const Board = ()=>{
+    return(
+        <div>
+            <div>
+                <Square value={1} />
+                <Square value={2} />
+                <Square value={3} />
+            </div>
+            <div>
+                <Square value={4} />
+                <Square value={5} />
+                <Square value={6} />
+            </div>
+            <div>
+                <Square value={7} />
+                <Square value={8} />
+                <Square value={9} />
+            </div>
+        </div>
+    );
+}
+export default Board;
