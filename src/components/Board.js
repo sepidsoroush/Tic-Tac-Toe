c8d601422ed319997921ac2449e7b0e2404bb7a1
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-14 16:15:11 +0300

    Add reset game button

--- src/components/Board.js
+++ src/components/Board.js
@@ -29,7 +29,8 @@ function calculateWinner (squares){
 }
 
 const Board = ()=>{
-    const [squares , setSquares] = useState(Array(9).fill(null))
+    const emptyBoard = Array(9).fill(null);
+    const [squares , setSquares] = useState(emptyBoard)
     const [xIsNext , setXIsNext] = useState(true)
     function handleClick(i){
         if(squares[i] || calculateWinner(squares)){
@@ -51,25 +52,35 @@ const Board = ()=>{
     }else{
         status = "Next Player :"+(xIsNext? 'X':'O');
     }
+    const Reset =({handleReset})=>{
+        return(
+            <button onClick={handleReset}>
+                Reset Game
+            </button>
+        )
+    }
 
     return(
         <div >
             <div className={styles.status}>{status}</div>
-            <div className={styles.boardRow}>
-                <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+            <div>
+                <div className={styles.boardRow}>
+                    <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+                    <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+                    <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+                </div>
+                <div className={styles.boardRow}>
+                    <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+                    <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+                    <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
+                </div>
+                <div className={styles.boardRow}>
+                    <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+                    <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+                    <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
+                </div>
             </div>
-            <div className={styles.boardRow}>
-                <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
-            </div>
-            <div className={styles.boardRow}>
-                <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
-            </div>
+            <div><Reset handleReset={()=>setSquares(emptyBoard)} /></div>
         </div>
     );
 }
