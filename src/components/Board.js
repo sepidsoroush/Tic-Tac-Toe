dc8b69af9c59b4505d401eaeb7d83843103847fa
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-15 13:23:01 +0300

    Add style to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -66,22 +66,30 @@ const Board = ()=>{
     }
 
     return(
-        <div >
+        <div className={styles.container}>
             <div className={styles.status}>{status}</div>
-            <div>
+            <div className={styles.board}>
                 <div className={styles.boardRow}>
                     <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
                 </div>
+                <div className={styles.rowBorder}></div>
                 <div className={styles.boardRow}>
                     <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
                 </div>
+                <div className={styles.rowBorder}></div>
                 <div className={styles.boardRow}>
                     <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+                    <span className={styles.colBorder}></span>
                     <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Board from '@/components/Board'
@@ -15,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className={`${styles.main} ${inter.className}`}>
         <Board />
       </main>
     </>
